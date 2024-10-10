1e6dbb698ecf78cc38b9d181ebe62e92e157a5cb
gabrielWYP <20222848@aloe.ulima.edu.pe>
2024-10-10 12:00:50 -0500

    commit

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -5,7 +5,7 @@ function Error({ statusCode }: { statusCode: number }) {
   return (
     <p>
       {statusCode
-        ? `An error ${statusCode} occurred on server`
+        ? 'An error ${statusCode} occurred on server'
         : 'An error occurred on client'}
     </p>
   );
